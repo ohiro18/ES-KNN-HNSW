25864fbbf6f4804615de7ad98f226202791ff137
ohiro18 <hironobu.ohara@g.softbank.co.jp>
2021-04-16 12:48:43 +0900

    first commit

---
+++ app/flask/app.py
@@ -0,0 +1,28 @@
+from flask import Flask, render_template
+from flask import request
+import es_operation
+
+app = Flask(__name__)
+
+ELASTIC_SERVER = '192.168.1.3:9200'
+ELASTIC_SERVER = 'elasticsearch:9200'
+es = es_operation.Ela(es_server=ELASTIC_SERVER)
+
+index_name = 'test_image'
+cates= es.get_category(index_name)
+
+@app.route('/')
+def index():
+  return render_template("index.html", title="FF14_image", cates=cates)
+
+@app.route('/pic', methods=['GET', 'POST'])
+def post():
+  cate = request.args.get('cate')
+  images = es.get_doc(index_name, cate) 
+  return render_template('index.html', \
+    title = 'FF14_image', \
+    message = '{}'.format(cate), cates=cates, selected=cate, images=images)
+
+
+if __name__ == '__main__':
+  app.run(host='0.0.0.0')
