351b27e94f4906a6568eb2f9b9b8ef6b254feb5f
ohiro18 <hironobu.ohara@g.softbank.co.jp>
2021-04-16 22:08:13 +0900

    KNNテスト

---
+++ app/insert.py
@@ -0,0 +1,55 @@
+from elasticsearch import Elasticsearch
+from datetime import datetime
+
+import pathlib
+import glob
+
+es = Elasticsearch(
+    hosts=[{'host': "es-sg-25u2403rq000qwfko.public.elasticsearch.aliyuncs.com", 'port':9200}],
+    http_auth=('elastic', 'FY21!hands-on'))
+
+res = es.cat.indices()
+
+
+img_path = pathlib.Path('C:\\Users\\1200358\\Dev\\ES-KNN-HNSW\\app\\flask\\static')
+
+for f in img_path.glob('*.jpg'):
+    print(f.name)
+
+    f_id = f.name.split('.')
+    
+    doc = { 
+        "image_name" : f.name, 
+        'timestamp': datetime.utcnow(), 
+        "category" : category
+        }
+
+    actions.append(
+        {
+            '_index':index_name, 
+            '_type':'_doc', 
+            '_id':f_id[0],  
+            '_source':doc
+            }
+            )
+
+from elasticsearch import helpers
+from datetime import datetime
+
+index_name = 'test_image'
+category = 'shadowbringers'
+
+# フォルダ内の画像リストを取得して、Elasticsearchに送る
+img_path = pathlib.Path('C:\\Users\\1200358\\Dev\\ES-KNN-HNSW\\app\\flask\\static')
+actions = []
+for f in img_path.glob('*.jpg'):
+    #print(f.name)
+    f_id = f.name.split('.')
+    
+    doc = { "image_name" : f.name, 'timestamp': datetime.utcnow(), "category" : category}
+    
+    actions.append({'_index':index_name, '_type':'_doc', '_id':f_id[0],  '_source':doc})
+
+
+helpers.bulk(es, actions)
+
