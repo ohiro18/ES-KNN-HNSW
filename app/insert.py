d362bf230916a417d581b44517784a40d3e59cb1
ohiro18 <hironobu.ohara@g.softbank.co.jp>
2021-04-22 23:45:40 +0900

    index変更

--- app/insert.py
+++ app/insert.py
@@ -5,32 +5,34 @@ import pathlib
 import glob
 
 es = Elasticsearch(
-    hosts=[{'host': "es-sg-25u2403rq000qwfko.public.elasticsearch.aliyuncs.com", 'port':9200}],
+    hosts=[{'host': "es-cn-mjc2443530001o0te.public.elasticsearch.aliyuncs.com", 'port':9200}],
     http_auth=('elastic', 'FY21!hands-on'))
 
-res = es.cat.indices()
+# ESでindex一覧を表示
+print(es.cat.indices())
 
-
+index_name = "web_app_test"
 img_path = pathlib.Path('C:\\Users\\1200358\\Dev\\ES-KNN-HNSW\\app\\flask\\static')
+dataset_dict = []
 
 for f in img_path.glob('*.jpg'):
-    print(f.name)
-
-    f_id = f.name.split('.')
-    
+    #print(f.name)
+    f_id = f.name.split('.')    
     doc = { 
         "image_name" : f.name, 
         'timestamp': datetime.utcnow(), 
-        "category" : category
         }
 
-    actions.append(
+    dataset_dict.append(
         {
             '_index':index_name, 
             '_type':'_doc', 
             '_id':f_id[0],  
             '_source':doc
-            }
-            )
+        }
+    )
+
+# ES indexへ一括インポート
+Elasticsearch.bulk(es, dataset_dict)
 
 
